a1fb70d845bc055343e5814129222bc49ea2de13
sudeen28 <polygrammy28@gmail.com>
2023-06-07 14:32:55 +0100

    first push on footer-section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+// import Footer from './components/Footer.jsx';
+import Footer2 from './components/Footer2.jsx';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      {/* <Footer/> */}
+      <Footer2/>
+  </div>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,93 @@
+// import React from "react";
+// import vect from "../Assets/icons/Vector (10).png";
+// import vect1 from "../Assets/icons/Vector (12).png";
+// import vect2 from "../Assets/icons/Vector (13).png";
+// import vect4 from "../Assets/icons/Vector (14).png";
+// import vect5 from "../Assets/icons/image 10.png";
+// import icon1 from "../Assets/icons/Vector (3).png";
+// import icon2 from "../Assets/icons/Vector (4).png";
+// import icon3 from "../Assets/icons/vector (5).svg";
+// import icon4 from "../Assets/icons/Vector (6).png";
+
+// const Footer = () => {
+//   return (
+//     <div>
+//       <footer className="footer">
+//         <div>
+//           <div className="vect1 d-flex justify-content-center">
+//             <img src={vect} alt="" />
+//           </div>
+//         </div>
+//         <div className="percent">
+//           <h1 className="">100%</h1>
+//           <h3 className="">Payment Secured</h3>
+//         </div>
+
+//         <div>
+//           <div className="vect d-flex  justify-content-center mt-4">
+//             <img src={vect1} alt="" />
+//             <img src={vect2} alt="" />
+//           </div>
+
+//           <div className="d-flex justify-content-center">
+//             <h1 className="twenty d-flex justify-content-center">24/7</h1>
+//             <h1 className="support d-flex justify-content-center">support</h1>
+//           </div>
+//         </div>
+//         <div></div>
+
+//         <div className="download">
+//           <div className="download-img">
+//             <img src={vect4} alt="" className="img-fluid img1" />
+//           </div>
+//           <div className="dwnld-text">
+//             <h1>Download on the</h1>
+//             <h1>APP STORE</h1>
+//           </div>
+//         </div>
+
+//         <div className="download">
+//           <div className="download-img">
+//             <img src={vect5} alt="" className="img-fluid img1" />
+//           </div>
+//           <div className="dwnld-text">
+//             <h1>Get it on </h1>
+//             <h1>GOOGLE PLAY</h1>
+//           </div>
+//         </div>
+
+//         <h1 className="help">Need help</h1>
+
+//         <div className="links d-flex ">
+//           <div className="link">
+//             <a href="tel:+234 466 6655">tel:+234 466 6655 or</a>
+//           </div>
+//           <div className="link">
+//             <a href="http://help@jazzyburger.com">help@jazzyburger.com</a>
+//           </div>
+//         </div>
+
+//         <h1 className="followus">FOLLOW US</h1>
+
+//         <div className="Sm-icons ">
+//           <div className="icons">
+//             <div>
+//               <img className="icon1" src={icon1} alt="" />
+//             </div>
+//             <div>
+//               <img className="icon2" src={icon2} alt="" />
+//             </div>
+//             <div>
+//               <img className="icon3" src={icon3} alt="" />
+//             </div>
+//             <div>
+//               <img className="icon4" src={icon4} alt="" />
+//             </div>
+//           </div>
+//         </div>
+//       </footer>
+//     </div>
+//   );
+// };
+
+// export default Footer;
