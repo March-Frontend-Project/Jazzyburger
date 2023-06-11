c9410a1e96d922153bf3135283cfd5190bf96d06
adelekeadegbenga <adelekesadiq@yahoo.com>
2023-06-11 12:07:45 -0700

    mobile responsive

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -4,7 +4,7 @@ import img1 from "../images/image 3.png"
 
 const LoginForm = () => {
     return (
-        <div className="cover col-8 offset-2 col-md-6 offset-md-3">
+        <div className="cover col-10 offset-1 col-md-6 offset-md-3">
             <img className="logo d-flex justify-content-center m-auto bg-light" src={img1} alt="" />
             <h6 className="d-flex justify-content-center m-auto pt-4">SIGN IN TO YOUR ACCOUNT</h6>
 
@@ -16,17 +16,20 @@ const LoginForm = () => {
                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="password" />
             </div><br></br>
 
-            <div className="d-flex flex-column align-items-center">
-                <div className="d-flex justify-content-between w-100">
-                    <div>
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
-                        <label class="form-check-label ps-1 mb-3 offset-col-10" for="exampleCheck1">Keep me signed in</label>
-                    </div>
-                    <label class="form-check-label text-primary col-md-1" for="exampleCheck1">Reset Password</label>
+            <div className="cover2 d-flex justify-content-between w-100">
+                <div>
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+                    <label class="form-check-label ps-1" for="exampleCheck1">Keep me signed in</label>
                 </div>
-                <button class="btn btn-danger text-light col-8 rounded w-100">Sign in</button>
-                <p className="">Dont have an account?<span className="text-primary"> Create one</span></p>
+
+                <a href="#">Reset Password</a>
             </div>
+
+
+            <button class="btn btn-danger text-light col-8 rounded w-100">Sign in</button>
+            <p className="text-center">Dont have an account?<a href="text-primary"> Create one</a></p>
+
+
 
 
         </div>
