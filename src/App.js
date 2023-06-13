846f4a1cf207729fa2ec06e5df042070b7858e03
ginowise <pascalu@gmail.com>
2023-06-13 16:17:53 +0100

    hero-banner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// import bannerImage from './images/herobanner.png';
+import desktopImg from '../src/images/herobanner.png';
+import mobileImg from '../src/images/mobile herobanner.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <img src={desktopImg} className="desktop-only" alt="..." />
+    <img src={mobileImg} className="mobile-only" alt="..."/>
     </div>
   );
 }
